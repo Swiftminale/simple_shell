2f52ec2dd0f394e7b0cdad4a18b30dc4520dd072
Swiftminale <minalefetene@gmail.com>
2023-12-11 21:11:15 -0800

    first commit

--- code_one.c
+++ code_one.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #define MAX_SIZE 100
-typedef struct
+/**
+* print_point - Prints the coordinates (x, y) of a point
+* @x: The x-coordinate of the point
+* @y: The y-coordinate of the point
+*/
+void print_point(int x, int y)
 {
-int x;
-int y;
-} Point;
-void print_point(Point *p)
-{
-printf("(%d, %d)\n", p->x, p->y);
+printf("(%d, %d)\n", x, y);
 }
-int main()
+/**
+* main - Entry point of the program
+* Return: 0 for successfull excution
+*/
+int main(void)
 {
-Point point;
-point.x = 5;
-point.y = 10;
-print_point(&point);
-return 0;
+int x = 5;
+int y = 10;
+/*Print the coordinates of the point*/
+print_point (x, y);
+return (0);
 }
